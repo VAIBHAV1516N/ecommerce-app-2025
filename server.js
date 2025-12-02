3d508355c14d23bf5c51eb4a70f2abf9af6ccbb4
Vaibhav Patil <150640895+VAIBHAV1516N@users.noreply.github.com>
2025-12-02 13:01:51 +0530

    Updated

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
 
-//rest api
-app.get('/.*/', (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
+// //rest api
+// app.get('/.*/', (req, res) {
+//   res.sendFile(path.join(__dirname, './client/build/index.html'));
+// });
 
 // GOOD:
 app.get(/.*/, (req, res) => {
