f5956cbbfbe543c087dcc6bdedecabd05380b60f
Vaibhav Patil <150640895+VAIBHAV1516N@users.noreply.github.com>
2025-12-02 11:27:51 +0530

    Updated

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
+import { fileURLToPath } from 'url';
 
 //config
 dotenv.config();
 
 //database connection
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest abject
 const app = express();
